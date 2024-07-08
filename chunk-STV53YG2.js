d2c1850a4e9ae0e151a87257ef0fd7bb0a7c9b95
yousraraed <yousraraed@hotmail.com>
2024-07-08 21:50:54 +0300

    Auto-generated commit

---
+++ chunk-STV53YG2.js
@@ -0,0 +1,1 @@
+import{X as t,Y as o}from"./chunk-E55XPUGU.js";var a=o("restaurant"),n=t(a,e=>e.restaurants),l=e=>t(n,r=>r.find(s=>s.id===+e)?.menus||[]),u=t(a,e=>e.loading),d=t(a,e=>e.error),i=e=>t(n,r=>r.find(s=>s.id===+e)?.name||[]);export{n as a,l as b,u as c,d,i as e};
